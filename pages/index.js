7af18ca2b4e59c7a556906ec4c5328923ffdde00
Test-SpringCloud <nicola_larocca@xantrix.it>
2021-09-18 16:46:50 +0200

    test useState concluso

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const InputElement = () => {
+
+    const [inputText,setInputText] = useState("");
+
+    return <div> 
+        <input onChange={(e) => {setInputText(e.target.value)} } placeholder="Inserisci la tua mail" /> 
+        <br/>
+        {inputText}
+        </div>
+};
+
+export default InputElement;

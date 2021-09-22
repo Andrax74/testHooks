0b0b65eacded275a69030efe305c5a0d51e68e3d
Test-SpringCloud <nicola_larocca@xantrix.it>
2021-09-22 17:00:27 +0200

    Primo uso usestate

--- pages/books.js
+++ pages/books.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import BooksCard from "../src/components/BooksCard";
 
@@ -9,6 +9,13 @@ const Books = () => {
         { title: 'Grishaverse - Assedio e tempesta', body: 'Ricercata per tutto il Mare Vero...', author: 'Leigh Bardugo', id: 2, image: '/static/images/book2.jpg' },
         { title: 'Grishaverse - Rovina e ascesa   ', body: 'L\'Oscuro ha ormai esteso...', author: 'Leigh Bardugo', id: 3, image: '/static/images/book3.jpg' }
     ])
+
+    const[nome, setNome] = useState("");
+
+    useEffect(() => {
+        console.log("use Effect attivato!")
+        console.log(libri);
+    },[nome]);
 
     const viewAllBooks = libri.map((libro) => (
         <BooksCard
@@ -27,9 +34,15 @@ const Books = () => {
     }
 
     return (
-        <div className="flex-card">
-            <h2>Libri Disponibili</h2>
-            {viewAllBooks}
+        <div>
+            <div className="flex-card">
+                <h2>Libri Disponibili</h2>
+                {viewAllBooks}
+            </div>
+            <div>
+                <button onClick={() => setNome("Nicola")}>Nome Lettore</button>
+                <p>{nome}</p>
+            </div>
         </div>
     );
 

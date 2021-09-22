73e6fbb2b741d54cf312eab805dd8c6919278436
Test-SpringCloud <nicola_larocca@xantrix.it>
2021-09-22 18:09:35 +0200

    Test Fetch con useEffect

--- pages/books.js
+++ pages/books.js
@@ -5,31 +5,43 @@ import BooksCard from "../src/components/BooksCard";
 const Books = () => {
 
     const [libri, setLibri] = useState([
+        { titolo: '', sinossi: '', autore: '', isbn: '', immagine: '' }
+    ]);
+    /*
+    const [libri, setLibri] = useState([
         { title: 'Grishaverse - Tenebre e ossa    ', body: 'Alina Starkov non ha grandi...', author: 'Leigh Bardugo', id: 1, image: '/static/images/book1.jpg' },
         { title: 'Grishaverse - Assedio e tempesta', body: 'Ricercata per tutto il Mare Vero...', author: 'Leigh Bardugo', id: 2, image: '/static/images/book2.jpg' },
         { title: 'Grishaverse - Rovina e ascesa   ', body: 'L\'Oscuro ha ormai esteso...', author: 'Leigh Bardugo', id: 3, image: '/static/images/book3.jpg' }
     ])
+    */
 
     const[nome, setNome] = useState("");
 
     useEffect(() => {
-        console.log("use Effect attivato!")
-        console.log(libri);
-    },[nome]);
+        console.log("Otteniamo i dati dalla web api");
+        
+        fetch('http://localhost:5051/api/libri')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setLibri(data);
+            })
+    },[]);
 
     const viewAllBooks = libri.map((libro) => (
         <BooksCard
-            key = {libro.id}
-            body = {libro.body}
-            title = {libro.title}
-            author = {libro.author}
-            image = {libro.image}
-            deleteBook = {() => handleDelete(libro.id)}
+            key = {libro.isbn}
+            body = {libro.sinossi}
+            title = {libro.titolo}
+            author = {libro.autore}
+            image = {"/static/images/" + libro.immagine}
+            deleteBook = {() => handleDelete(libro.isbn)}
         />
     ))
 
     const handleDelete = (id) => {
-        const newBooks = libri.filter(libri => libri.id !== id);
+        const newBooks = libri.filter(libri => libri.isbn !== id);
         setLibri(newBooks);
     }
 
